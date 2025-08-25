7ca33e63a652b27b8a42edbb2db5cbea7fcc91bc
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-08-25 20:33:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+    
+        <Route path="/ragistration" element={<Home />}></Route>
+        <Route path="/login" element={<Shop />}></Route>
+        <Route path="/home" element={<ProductDetails />}></Route>
+        
+    </>
+  )
+);
+    </>
+  )
+}
+
+export default App
