f8ebf59761535b61a8dfe2d6470ea7ee7339289a
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-08-25 22:06:32 +0600

    login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GoogleAuthProvider } from "firebase/auth";
 import { getAuth, signInWithPopup } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 function Login() {
  const provider = new GoogleAuthProvider();
+const [password,setPassword]=useState([])
+const [email,setEmail]=useState([])
+
+let handleEmail=(e)=>{
+setEmail(e.target.value)
+}
+
+let handlePassword=(e)=>{
+setPassword(e.target.value)
+}
+ let handlelogin=()=>{
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    
+
+ 
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+  });
+
+ }
   
   let handleGoogleAuth=()=>{
    
@@ -23,17 +46,17 @@ signInWithPopup(auth, provider)
   }
   return (
     <div>
-      <button className='border-4 m-10' onClick={handleGoogleAuth} >loginwithe google</button>
+      <button className='border-4 m-10 bg-yellow-300' onClick={handleGoogleAuth} >loginwithe google</button>
        <div className='border-5'>
       <label>login
-        <input className="bg-slate-300" type="text" />
+        <input onChange={handleEmail} className="bg-slate-300" type="text" />
         
       </label>
       <label>paswowed
-        <input className="bg-slate-300" type="text" />
+        <input onChange={handlePassword} className="bg-slate-300" type="text" />
       </label>
     
-       <button>Ragistration</button>
+       <button onClick={handlelogin}>login</button>
 
     </div>
     </div>
